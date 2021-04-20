76557c7cded29d801ff5b24ab051ca87372f0675
pedro <pedroscampoy@gmail.com>
2021-04-21 00:02:33 +0200

    initial internal body and organ component

---
+++ src/components/anatomy/Organ.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Organ = ({children}) => {
+    return (
+        <React.Fragment>
+            {children}
+        </React.Fragment>
+    );
+  }
+  
+  export default Organ;
