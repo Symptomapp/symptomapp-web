3fd8bd6cab0559071a9639474adecdabfaad4f4a
monteromario <59968975+monteromario@users.noreply.github.com>
2021-05-04 21:04:00 +0200

    Profile and search improvements

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,7 @@
 import { React, useEffect, useState } from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+import Table from 'react-bootstrap/Table'
 import symptoms_json from '../data/symptoms.json'
 
 const symptoms = symptoms_json.map(symptom => {
@@ -21,19 +24,37 @@ const Search = () => {
     }, [searchTerm]);
 
   return (
-    <div className="container m-5">
-      <input
-        type="text"
-        placeholder="Search"
-        value={searchTerm}
-        onChange={handleChange}
-      />
-      <ul className="mt-3">
-         {searchResults.map(item => (
-          <li>{item}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+    <Jumbotron fluid className="mb-5">
+      <Container>
+          <h1>Symptom search</h1>
+          <p>
+          Search for any particular symptom. You can use the search bar below to filter results.
+          </p>
+          <div className="d-flex justify-content-center h-100 mb-5">
+            <div className="searchbar mt-3">
+              <input
+                className="search_input" 
+                type="text"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={handleChange}
+              />
+            <div className="search_icon"><i className="fas fa-search"></i></div>
+            </div>
+          </div>
+          <Table>
+            <tbody>
+              {searchResults.map(item => (
+                <tr key={item}>
+                <td className="search--row">{item}</td>
+              </tr>
+              ))}
+            </tbody>
+          </Table>
+      </Container>
+    </Jumbotron>
+    </>
   );
 }
 
